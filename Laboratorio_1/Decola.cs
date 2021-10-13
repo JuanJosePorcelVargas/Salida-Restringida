1b2227464d8deff58225273480b9ab14b32fe35b
Juan Jose Porcel Vargas <juanjoseporcel11@gmail.com>
2021-10-13 17:14:05 -0400

    Agregar archivos de proyecto.

---
+++ Laboratorio_1/Decola.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio_1
+{
+    class Decola
+    {
+        static int maxCola = 6;
+        public int primero;
+        public int ultimo;
+        public int[] lista;
+
+
+        public Decola()
+        {
+            primero = 0;
+            ultimo = -1;
+            lista = new int[maxCola];
+        }
+
+        public bool vacia()
+        {
+            if (ultimo < primero)
+                return true;
+            else
+
+                return false;
+
+
+
+
+        }
+        public bool llena()
+        {
+            if (ultimo == maxCola - 1)
+                return true;
+            else
+                return false;
+
+        }
+        
+
+
+                                                                  //TAREA
+
+        public void insertar_primero(int elemento)
+        {
+
+            if (llena())
+                MessageBox.Show("La cola esta llena,no puede insertar mas elementos");
+            else
+            {
+              
+                for(int i = ultimo; i >= 0; i--)
+                {
+
+                    lista[i + 1] = lista[i];
+                 
+                }
+                
+                lista[primero] = elemento;
+                ultimo++;
+
+
+
+            }
+        }
+
+        public void insertar_ultimo(int elemento)
+        {
+            if (llena())
+                MessageBox.Show("La cola esta llena,No puede insertar mas elementos");
+            else
+            {
+                ultimo++;
+                lista[ultimo] = elemento;
+
+            }
+
+        }
+
+        public void eliminar_ultimo()
+        {
+            if (vacia())
+                MessageBox.Show("La cola esta vacia,no puede eliminar mas elementos");
+            else
+            {
+                MessageBox.Show("El elemento a eliminar es: " + lista[ultimo]);
+                ultimo--;
+
+
+            }
+
+
+
+
+        }
+       
+
+
+    }
+
+
+}
+
---
+++ Laboratorio_1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio_1
+{
+    public partial class Form1 : Form
+    {
+        Decola mensajero = new Decola();
+
+        public void mostrarCola()
+        {
+            listView1.Clear();
+            for (int i = 0; i <= mensajero.ultimo; i++)
+            {
+                listView1.Items.Add(mensajero.lista[i].ToString());
+
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mensajero.insertar_primero(Int32.Parse(textBox1.Text));
+            textBox1.Clear();
+            textBox1.Focus();
+            mostrarCola();
+
+        }
+
+       
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            mensajero.eliminar_ultimo();
+            mostrarCola();
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mensajero.insertar_ultimo(Int32.Parse(textBox1.Text));
+            textBox1.Clear();
+            textBox1.Focus();
+            mostrarCola();
+        }
+    }
+}
